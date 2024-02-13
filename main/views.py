6d7df474a7ca96f86880696dc52a3ef190d448f2
Pokshubina Sofia <spokshubina@digital-spirit.ru>
2024-02-14 01:20:24 +0300

    Добавила главную и страницу с информацией

--- main/views.py
+++ main/views.py
@@ -3,10 +3,15 @@ from django.template import context
 
 def index(request):
     context = {
-        'title': 'Home',
-        'content': 'Главная страница'
+        'title': 'FlowerAI - Главная',
+        'content': 'Создай свой уникальный букет'
     }
     return render(request, 'main/index.html', context)
 
 def about(request):
-    return render(request, 'About')
+    context = {
+        'title': 'FlowerAI - О нас',
+        'content': 'О нас',
+        'text_on_page': 'Text'
+    }
+    return render(request, 'main/about.html', context)
