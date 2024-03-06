835ce9f1361ed3a7f519cdf729340de9da7bb287
Pokshubina Sofia <spokshubina@digital-spirit.ru>
2024-03-07 02:42:36 +0300

    Added pagination

--- goods/views.py
+++ goods/views.py
@@ -1,13 +1,29 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.shortcuts import get_list_or_404, render
 from goods.models import Products
 
-def catalog(request):
-    goods = Products.objects.all()
+def catalog(request, category_slug):
+
+    page = request.GET.get('page', 1)
+
+    if category_slug == 'all':
+        goods = Products.objects.all()
+    else:
+        goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
+    
+    paginator = Paginator(goods, 9)
+    current_page = paginator.page(int(page))
+
     context = {
         'title': 'FlowerAI - Каталог',
-        'goods': goods,
+        'goods': current_page,
+        'slug_url': category_slug
     }
     return render(request, 'goods/catalog.html', context)
 
-def product(request):
-    return render(request, 'goods/product.html')
+def product(request, product_slug):
+    product = Products.objects.get(slug=product_slug)
+    context = {
+        'product': product
+    }
+    return render(request, 'goods/product.html', context=context)
